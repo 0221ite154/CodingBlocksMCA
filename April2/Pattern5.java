b4c1bb78e47efba2c874abc4ad8c00db670f1eee
0221ITE154 <0221ITE154@gmail.com>
2026-04-02 16:14:52 +0530

    init commit

--- April2/Pattern5.java
+++ April2/Pattern5.java
@@ -1,13 +1,16 @@
 package April2;
 /*
  *      n=3
- *      ***
- *      ***
+ *      *
+ *      **
  *      ***
  */
 public class Pattern5 {
 	public static void main(String args[]) {
+		// with mantra
 		
+		// outer for loop will change the line
+		// inner for loop will print the pattern
 		int n =5;
 		for(int i=1;i<=n;i++) {
 			for(int j=1;j<=i;j++) {
