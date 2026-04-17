01c2283150b04f9e11894a3a6abea4855579b1c3
0221ITE154 <0221ITE154@gmail.com>
2026-04-17 11:28:29 +0530

    init commit

---
+++ April_17/Pattern24.java
@@ -0,0 +1,36 @@
+package April_17;
+
+public class Pattern24 {
+	
+	/*
+n=5
+    1
+   212
+  32123
+ 4321234
+543212345
+
+
+
+
+	 */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n =5;
+		for(int i=1;i<=n;i++) {
+			for(int j=1 ; j<=n-i;j++) {
+				System.out.print(" ");
+			}
+			for(int j=i;j>=1;j--) {
+				System.out.print(j);
+			}
+			for(int j=2;j<=i;j++) {
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+
+	}
+
+}
